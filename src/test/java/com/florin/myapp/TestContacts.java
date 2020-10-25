e52267c24f9dd07a2ed82a278c171fb5a312adc2
zamfirflorin <zamfirflorinstelian@yahoo.com>
2020-10-25 15:53:57 +0200

    -- added org.junit.Test

--- src/test/java/com/florin/myapp/TestContacts.java
+++ src/test/java/com/florin/myapp/TestContacts.java
@@ -3,7 +3,7 @@ package com.florin.myapp;
 import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import com.florin.myapp.contacts.Contacts;
 
