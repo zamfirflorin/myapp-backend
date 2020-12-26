1bc1a1c175af2756256e6775c69529a5e09c55d7
zamfirflorin <zamfirflorinstelian@yahoo.com>
2020-12-27 00:59:46 +0200

    updated rest methods

--- src/main/java/com/florin/myapp/controller/ContactsController.java
+++ src/main/java/com/florin/myapp/controller/ContactsController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -15,10 +16,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+
 import com.florin.myapp.contacts.Contacts;
 import com.florin.myapp.repository.ContactsRepository;
-
+import java.net.URI;
+import java.net.URISyntaxException;
 /**
  * @author Florin
  *
@@ -31,56 +33,66 @@ public class ContactsController {
 	
 	@Autowired
 	ContactsRepository contactsRepository; 
-	//CREATE / INSERT / POST
-	
-	/*
-	 * @GetMapping(path="/helloworld") public String helloWorld() { return
-	 * "Helloworld againada"; }
-	 */
-	
 	
 	//RETRIEVE/READ / SELECT /GET
 	@GetMapping(path="/contacts")
 	public ResponseEntity<List<Contacts>> getAllContacts(){
-		
+
 		return ResponseEntity.ok(contactsRepository.findAll());
 	}
 	
-	@PostMapping(path="/add")
-	public Contacts createContact(@RequestBody String firstName, @RequestBody String lastName){
-		
-		Contacts contact = new Contacts();
-		contact.setFirstName(firstName);
-		contact.setLastName(lastName);
-		return contactsRepository.save(contact);
-		
+	@GetMapping(path="/contacts/{contactId}")
+	public ResponseEntity<Contacts> getContactById(@PathVariable long contactId){
+
+		try {
+			Contacts contact = contactsRepository.findById(contactId);
+			return ResponseEntity.ok(contact);
+		} catch (ResourceNotFoundException ex) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+
+		}
+	}
+	
+	//CREATE / INSERT / POST	
+	@PostMapping(path="/contacts/add")
+	public ResponseEntity<Contacts> createContact(@RequestBody Contacts contact){
+
+		Contacts newContact = contactsRepository.save(contact);
+
+		try {
+			return ResponseEntity.created(new URI("/contacts/add" + newContact.getId())).body(contact);
+		} catch (URISyntaxException ex) {
+	        // log exception first, then return Conflict (409)
+	        logger.error(ex.getMessage());
+	        return ResponseEntity.status(HttpStatus.CONFLICT).build();
+	    } 
+
 	}
 	
 
 	//UPDATE / UPDATE/ REPLACE PUT
 	
-	@PutMapping(path="/update/{id}")
+	@PutMapping(path="/contacts/update/{id}")
 	public ResponseEntity<Contacts> updateContact(@PathVariable long id, @RequestBody String firstName, @RequestBody String lastName){
-		
-		
+
 		Contacts contact = contactsRepository.findById(id);
+		contact.setId(id);
 		contact.setFirstName(firstName);
 		contact.setLastName(lastName);
-		
-		return new ResponseEntity<Contacts>(contact, HttpStatus.OK);
-		
+		return  ResponseEntity.ok().build();
+
 	}
 	
 	//DELETE / DELETE / DEleteW
 	@DeleteMapping(path="/delete/{id}")
 	public ResponseEntity<Void> deleteContact(@PathVariable long id) {
-		
+
 		try {
-		contactsRepository.deleteById(id);
-		return ResponseEntity.ok().build();
+			contactsRepository.deleteById(id);
+			return ResponseEntity.ok().build();
 		} catch(ResourceNotFoundException ex) {
-			 logger.error(ex.getMessage());
-		    return ResponseEntity.notFound().build();
+			logger.error(ex.getMessage());
+			return ResponseEntity.notFound().build();
 		}
 
 	}
